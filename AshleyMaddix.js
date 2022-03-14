b30d2639679e9bd9d13970f6efa43e5fe73ee8fa
amaddix <60373214+amaddix@users.noreply.github.com>
2022-03-13 20:09:25 -0700

    Update AshleyMaddix.js
    adding submit button function for contact me page

--- AshleyMaddix.js
+++ AshleyMaddix.js
@@ -1,1 +1,14 @@
 
+let form = document.querySelector("form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(document.getElementById("fname").value);
+  console.log(document.getElementById("lname").value);
+
+  console.log(document.getElementById("email").value);
+  console.log(document.getElementById("phone").value);
+}
+  
